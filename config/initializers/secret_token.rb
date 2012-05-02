32f87594cb252d9198c2549c9d250fba19580922
Adam Pisoni <apisoni@yammer-inc.com>
2012-05-02 13:18:52 -0700

    rails biatch!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flipcode::Application.config.secret_token = '44f1ee0686e701b5a4b9537b7f693d6ab4bbdfe92c24e8e83f36451b3d5b9db103bb7739ea1d393f4bf3646d4a4ac74ea5a03bb8740c3adba8451e0d113bc297'
