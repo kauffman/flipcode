fb9a14ebce285ab64ce8d8ad85298cb29729ce44
Adam Pisoni <apisoni@yammer-inc.com>
2012-05-02 18:44:08 -0700

    rename Flip to BaseFlip

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,7 +13,7 @@ class Game < ActiveRecord::Base
 private
 
   def set_up_flips
-    Flip.flips.shuffle.first(player_count).each do |flip|
+    BaseFlip.flips.shuffle.first(player_count).each do |flip|
       flips.create(:name => flip.to_s)
     end
   end
