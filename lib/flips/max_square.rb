49fe9807991e26c92bff6471583661027b3cfecc
Kris Gale <krisgale@gmail.com>
2012-05-02 18:44:16 -0700

    HELL YEAH

---
+++ lib/flips/max_square.rb
@@ -0,0 +1,20 @@
+class MaxSquare < BaseFlip
+
+  desc 'Given a list of integers, return the highest perfect square. There will always be squares in the input.'
+    
+  def input_generator
+    input = []
+    10.times do
+      input << rand(1500)
+    end
+    2.times do
+      input << (5..38).to_a.rand ** 2
+    end
+    return input
+  end
+
+  def valid_output
+    input.inject(0) { |max, i| i > max.to_i && (Math.sqrt(i) == Math.sqrt(i).to_i) ? i : max }
+  end
+
+end
